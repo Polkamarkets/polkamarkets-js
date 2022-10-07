d292a5f6f96d8d1421ed07e36df820169bc8f67f
Andre Goncalves <andrefcgoncalves@gmail.com>
2022-10-07 07:17:01 +0100

    Added some comments

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -10,7 +10,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 /// @title Upvoting / Downvoting Contract
 contract Voting {
   using SafeMath for uint256;
-  /// @dev protocol is immutable and has no ownership
 
   event ItemVotesUpdated(
     uint256 indexed itemId,
@@ -45,11 +44,16 @@ contract Voting {
   IERC20 public token;
   uint256 public requiredBalance; // required balance for voting
 
+  // ------ Modifiers ------
+
   modifier mustHoldRequiredBalance() {
     require(token.balanceOf(msg.sender) >= requiredBalance, "msg.sender must hold minimum erc20 balance");
     _;
   }
 
+  // ------ Modifiers End ------
+
+  /// @dev protocol is immutable and has no ownership
   constructor(
     IERC20 _token,
     uint256 _requiredBalance
@@ -58,14 +62,17 @@ contract Voting {
     requiredBalance = _requiredBalance;
   }
 
+  /// @dev allows user to upvote an item
   function upvoteItem(uint256 itemId) mustHoldRequiredBalance external {
     voteItem(itemId, VotesAction.upvoted);
   }
 
+  /// @dev allows user to downvote an item
   function downvoteItem(uint256 itemId) mustHoldRequiredBalance external {
     voteItem(itemId, VotesAction.downvoted);
   }
 
+  /// @dev logic of upvoting/downvoting an item
   function voteItem(uint256 itemId, VotesAction action) private {
     require(action == VotesAction.upvoted || action == VotesAction.downvoted, "Function parameter has to be upvoted or downvoted");
 
@@ -99,14 +106,17 @@ contract Voting {
 
   }
 
+  /// @dev allows user to remove an upvote from an item
   function removeUpvoteItem(uint256 itemId) mustHoldRequiredBalance external {
     removeVoteItem(itemId, VotesAction.removeUpvoted);
   }
 
+  /// @dev allows user to remove a downvote from an item
   function removeDownvoteItem(uint256 itemId) mustHoldRequiredBalance external {
     removeVoteItem(itemId, VotesAction.removeDownvoted);
   }
 
+  /// @dev logic of removing an upvote/downvote from an item
   function removeVoteItem(uint256 itemId, VotesAction action) private {
     require(action == VotesAction.removeUpvoted || action == VotesAction.removeDownvoted, "Function parameter has to be removeUpvoted or removeDownvoted");
 
@@ -130,12 +140,14 @@ contract Voting {
     emit ItemVotesUpdated(itemId, item.upvotes, item.downvotes, now);
   }
 
+  /// @dev Returns the number of upvotes/downvotes of an item
   function getItemVotes(uint256 itemId) external view returns (uint256, uint256) {
     ItemVotes storage item = items[itemId];
 
     return (item.upvotes, item.downvotes);
   }
 
+  /// @dev Returns info if the user has a upvote or a downvote in an item
   function hasUserVotedItem(address user, uint256 itemId) external view returns (bool, bool) {
     ItemVotes storage item = items[itemId];
 
