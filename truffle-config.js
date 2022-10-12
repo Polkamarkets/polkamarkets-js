659fb3082da3e32769f0645d53b93e25b7a1b95d
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-10-13 00:31:04 +0100

    fix(truffle): remove deprecated testnets; add goerli testnet; infura api key from env var

--- truffle-config.js
+++ truffle-config.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const mnemonic = process.env.TRUFFLE_MNEMONIC;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
 
 module.exports = {
   networks: {
@@ -11,7 +12,7 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     live: {
-      provider: new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/37ec248f2a244e3ab9c265d0919a6cbc"),
+      provider: new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/" + INFURA_API_KEY),
       network_id: 1,
       gasPrice: 100000000000,
       websockets: true
@@ -24,14 +25,9 @@ module.exports = {
       provider: new HDWalletProvider(mnemonic, "https://rpc.api.moonbeam.network"),
       network_id: 1284
     },
-    kovan: {
-      provider: new HDWalletProvider(mnemonic, "https://kovan.infura.io/v3/37ec248f2a244e3ab9c265d0919a6cbc"),
-      network_id: 42
-    },
-    rinkeby: {
-      provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/37ec248f2a244e3ab9c265d0919a6cbc"),
-      network_id: 4,
-      gas: 5000000
+    goerli: {
+      provider: new HDWalletProvider(mnemonic, "https://goerli.infura.io/v3/" + INFURA_API_KEY),
+      network_id: 5
     },
     leprichain: {
       provider: new HDWalletProvider(mnemonic, "https://node.leprichain.blockwell.ai"),
