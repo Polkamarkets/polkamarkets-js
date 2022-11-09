740b44bf4d0a3b9b229e971651aaa9bdc0210fee
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-11-09 23:04:53 +0000

    add predictionMarketV2 migration

---
+++ migrations/8_deploy_pm_v2.js
@@ -0,0 +1,20 @@
+const PredictionMarketV2 = artifacts.require("PredictionMarketV2");
+const {
+  FEE,
+  TOKEN,
+  REQUIRED_BALANCE,
+  REALITIO_ADDRESS,
+  REALITIO_TIMEOUT,
+} = process.env;
+
+
+module.exports = async function(deployer) {
+  await deployer.deploy(
+    PredictionMarketV2,
+    FEE, // fee
+    TOKEN, // token
+    REQUIRED_BALANCE, // requiredBalance
+    REALITIO_ADDRESS, // realitioAddress
+    REALITIO_TIMEOUT, // realitioTimeout,
+  );
+};
