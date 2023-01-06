997149fb0388412ff404726baa1be358e842e84c
Ricardo Marques <ricardo.ds.marques@gmail.com>
2023-01-06 17:23:26 +0000

    prettier: contracts

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,29 +1,17 @@
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
-
 // openzeppelin imports
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
 
 /// @title Upvoting / Downvoting Contract
 contract Voting {
   using SafeMath for uint256;
 
-  event ItemVotesUpdated(
-    uint256 indexed itemId,
-    uint256 upvotes,
-    uint256 downvotes,
-    uint256 timestamp
-  );
+  event ItemVotesUpdated(uint256 indexed itemId, uint256 upvotes, uint256 downvotes, uint256 timestamp);
 
-  event ItemVoteAction(
-    address indexed user,
-    VoteAction indexed action,
-    uint256 indexed itemId,
-    uint256 timestamp
-  );
+  event ItemVoteAction(address indexed user, VoteAction indexed action, uint256 indexed itemId, uint256 timestamp);
 
   enum VoteAction {
     upvote,
@@ -54,32 +42,36 @@ contract Voting {
   // ------ Modifiers End ------
 
   /// @dev protocol is immutable and has no ownership
-  constructor(
-    IERC20 _token,
-    uint256 _requiredBalance
-  ) public {
+  constructor(IERC20 _token, uint256 _requiredBalance) public {
     token = _token;
     requiredBalance = _requiredBalance;
   }
 
   /// @dev allows user to upvote an item
-  function upvoteItem(uint256 itemId) mustHoldRequiredBalance external {
+  function upvoteItem(uint256 itemId) external mustHoldRequiredBalance {
     voteItem(itemId, VoteAction.upvote);
   }
 
   /// @dev allows user to downvote an item
-  function downvoteItem(uint256 itemId) mustHoldRequiredBalance external {
+  function downvoteItem(uint256 itemId) external mustHoldRequiredBalance {
     voteItem(itemId, VoteAction.downvote);
   }
 
   /// @dev logic of upvoting/downvoting an item
   function voteItem(uint256 itemId, VoteAction action) private {
-    require(action == VoteAction.upvote || action == VoteAction.downvote, "Function parameter has to be upvoted or downvoted");
+    require(
+      action == VoteAction.upvote || action == VoteAction.downvote,
+      "Function parameter has to be upvoted or downvoted"
+    );
 
     ItemVotes storage item = items[itemId];
 
-    mapping(address => bool) storage listToAddVote = action == VoteAction.upvote ? item.usersUpvoted : item.usersDownvoted;
-    mapping(address => bool) storage listToRemoveVote = action == VoteAction.upvote ? item.usersDownvoted : item.usersUpvoted;
+    mapping(address => bool) storage listToAddVote = action == VoteAction.upvote
+      ? item.usersUpvoted
+      : item.usersDownvoted;
+    mapping(address => bool) storage listToRemoveVote = action == VoteAction.upvote
+      ? item.usersDownvoted
+      : item.usersUpvoted;
 
     // check if user has not already voted on the selected direction
     bool hasUserVotedSelectedDirection = listToAddVote[msg.sender];
@@ -103,27 +95,31 @@ contract Voting {
     emit ItemVoteAction(msg.sender, action, itemId, now);
 
     emit ItemVotesUpdated(itemId, item.upvotes, item.downvotes, now);
-
   }
 
   /// @dev allows user to remove an upvote from an item
-  function removeUpvoteItem(uint256 itemId) mustHoldRequiredBalance external {
+  function removeUpvoteItem(uint256 itemId) external mustHoldRequiredBalance {
     removeVoteItem(itemId, VoteAction.removeUpvote);
   }
 
   /// @dev allows user to remove a downvote from an item
-  function removeDownvoteItem(uint256 itemId) mustHoldRequiredBalance external {
+  function removeDownvoteItem(uint256 itemId) external mustHoldRequiredBalance {
     removeVoteItem(itemId, VoteAction.removeDownvote);
   }
 
   /// @dev logic of removing an upvote/downvote from an item
   function removeVoteItem(uint256 itemId, VoteAction action) private {
-    require(action == VoteAction.removeUpvote || action == VoteAction.removeDownvote, "Function parameter has to be removeUpvoted or removeDownvoted");
+    require(
+      action == VoteAction.removeUpvote || action == VoteAction.removeDownvote,
+      "Function parameter has to be removeUpvoted or removeDownvoted"
+    );
 
     // get item votes
     ItemVotes storage item = items[itemId];
 
-    mapping(address => bool) storage listToRemoveVote = action == VoteAction.removeUpvote ? item.usersUpvoted : item.usersDownvoted;
+    mapping(address => bool) storage listToRemoveVote = action == VoteAction.removeUpvote
+      ? item.usersUpvoted
+      : item.usersDownvoted;
 
     // check if user has voted item
     bool hasUserVoted = listToRemoveVote[msg.sender];
@@ -153,5 +149,4 @@ contract Voting {
 
     return (item.usersUpvoted[user], item.usersDownvoted[user]);
   }
-
 }
