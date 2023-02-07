92b871e43bf77d2e405c8813d2109c95e245c0ca
Ricardo Marques <ricardo.ds.marques@gmail.com>
2023-02-07 15:43:29 +0000

    update migrations

--- migrations/2_deploy_erc20.js
+++ migrations/2_deploy_erc20.js
@@ -1,10 +1,10 @@
 const PredictionMarket = artifacts.require("PredictionMarket");
 const ERC20PresetMinterPauser = artifacts.require("ERC20PresetMinterPauser");
-const USER = process.env.USER;
+const ERC20_USER = process.env.ERC20_USER;
 
 module.exports = async function(deployer, network, accounts) {
   // NOTE: USE THIS ONLY FOR DEV PURPOSES!!
-  await deployer.deploy(ERC20PresetMinterPauser, "Polkamarkets", "POLK");
+  await deployer.deploy(ERC20PresetMinterPauser, "Test Token", "TEST");
   const token = await ERC20PresetMinterPauser.deployed();
-  await token.mint(USER, '100000000000000000000000000');
+  await token.mint(ERC20_USER, '100000000000000000000000000');
 }
