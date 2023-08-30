be88323055978afa2dfe2ae4057967be54811b0d
Ricardo Marques <ricardo.ds.marques@gmail.com>
2023-08-30 13:48:12 +0100

    feat(PM2): remove legacy code

--- src/models/PredictionMarketV2Contract.js
+++ src/models/PredictionMarketV2Contract.js
@@ -303,29 +303,7 @@ class PredictionMarketV2Contract extends IContract {
       } else {
         const decimals = await this.getMarketDecimals({marketId});
         const marketShares = await this.getContract().methods.getUserMarketShares(marketId, user).call();
-        let claimStatus;
-        try {
-          claimStatus = await this.getContract().methods.getUserClaimStatus(marketId, user).call();
-        } catch (err) {
-          // SafeMath subtraction overflow error from Moonriver deployment
-          if (err.message.includes('SafeMath: subtraction overflow')) {
-            claimStatus = [false, false, false, false, 0];
-
-            const marketData = await this.params.contract.getContract().methods.getMarketData(marketId).call();
-            if (parseInt(marketData[0]) === 2) {
-              // market resolved, computing if user has winnings to claim
-              claimStatus[0] = marketShares[1][parseInt(marketData[5])] > 0;
-              if (claimStatus[0]) {
-                const events = await this.getEvents('MarketActionTx', { marketId, user, action: 4 });
-                claimStatus[1] = events.length > 0;
-              }
-              claimStatus[2] = marketShares[0] > 0;
-              claimStatus[3] = claimStatus[2];
-            }
-          } else {
-            throw err;
-          }
-        }
+        const claimStatus = await this.getContract().methods.getUserClaimStatus(marketId, user).call();
 
         const outcomeShares = Object.fromEntries(marketShares[1].map((item, index) => {
           return [
