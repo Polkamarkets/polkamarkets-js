82d082ae51b6a414fb59370281dae3d4b479eaf9
Ricardo Marques <ricardo.ds.marques@gmail.com>
2024-01-12 21:47:28 -0300

    feat(v3): add PredictionMarketV3 test suite (1st batch)

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,7 @@
 context('Unit Tests', async () => {
   require('./predictionMarketContract');
   require('./predictionMarketV2Contract');
+  require('./predictionMarketV3Contract');
   require('./fantasyERC20Contract');
   require('./votingContract');
 });
