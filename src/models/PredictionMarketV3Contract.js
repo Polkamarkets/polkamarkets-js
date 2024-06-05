129d118b760a31ecfb0c700b7e7f4a0b0b1c4a71
Ricardo Marques <ricardo.ds.marques@gmail.com>
2024-06-05 19:22:53 +0100

    fix(PredictionMarketV3Querier): add model and getPortfolio function to PredictionMarketV3

--- src/models/PredictionMarketV3Contract.js
+++ src/models/PredictionMarketV3Contract.js
@@ -1,5 +1,8 @@
 const predictionV3 = require("../interfaces").predictionV3;
 const PredictionMarketV2Contract = require("./PredictionMarketV2Contract");
+const PredictionMarketV3QuerierContract = require("./PredictionMarketV3QuerierContract");
+
+const Numbers = require('../utils/Numbers');
 
 class PredictionMarketV3Contract extends PredictionMarketV2Contract {
   constructor(params) {
@@ -8,6 +11,12 @@ class PredictionMarketV3Contract extends PredictionMarketV2Contract {
     if (params.isSocialLogin) {
       // social login tokens are standard ERC20 tokens
       this.marketDecimals = 18;
+    }
+    if (params.querierContractAddress) {
+      this.querier = new PredictionMarketV3QuerierContract({
+        ...this.params,
+        contractAddress: params.querierContractAddress
+      });
     }
   }
 
@@ -146,6 +155,70 @@ class PredictionMarketV3Contract extends PredictionMarketV2Contract {
       this.getContract().methods.adminResolveMarketOutcome(marketId, outcomeId),
       false,
     );
+  };
+
+  async getPortfolio({ user }) {
+    if (!this.querier) {
+      return super.getPortfolio({ user });
+    }
+
+    let events = [];
+    try {
+      events = await this.getActions({ user });
+    } catch (err) {
+      // should be non-blocking
+    }
+
+    const userMarketsData = await this.querier.getUserAllMarketsData({ user });
+    const marketIds = Object.keys(userMarketsData).map(Number);
+    const decimals = 18;
+
+    const portfolio = marketIds.reduce((obj, marketId) => {
+      const marketData = userMarketsData[marketId];
+
+      const outcomeShares = Object.fromEntries(marketData.outcomeShares.map((item, index) => {
+        return [
+          index,
+          {
+            shares: Numbers.fromDecimalsNumber(item, decimals),
+            price: this.getAverageOutcomeBuyPrice({events, marketId, outcomeId: index})
+          }
+        ];
+      }));
+
+      const item = {
+        liquidity: {
+          shares: Numbers.fromDecimalsNumber(marketData.liquidityShares, decimals),
+          price: this.getAverageAddLiquidityPrice({events, marketId}),
+        },
+        outcomes: outcomeShares,
+        claimStatus: {
+          winningsToClaim: marketData.winningsToClaim,
+          winningsClaimed: marketData.winningsClaimed,
+          liquidityToClaim: marketData.liquidityToClaim,
+          liquidityClaimed: marketData.liquidityClaimed,
+          voidedWinningsToClaim: marketData.voidedSharesToClaim,
+          voidedWinningsClaimed: false,
+          liquidityFees: 0 // discontinued
+        }
+      };
+
+      return {
+        ...obj,
+        [marketId]: item
+      };
+    }, {});
+
+    // calculationg voidedWinningsClaimed, if there's any voidedWinningsToClaim
+    Object.keys(portfolio).forEach(marketId => {
+      const item = portfolio[marketId];
+      if (item.claimStatus.voidedWinningsToClaim) {
+        item.claimStatus.voidedWinningsClaimed =
+          events.some(event => event.action === 'Claim Voided' && event.marketId === marketId);
+      }
+    });
+
+    return portfolio;
   };
 }
 
