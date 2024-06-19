64c931d71163e25caad85928b1c941a93c2a8961
Andre Goncalves <andrefcgoncalves@gmail.com>
2024-06-19 19:26:58 +0100

    Fix tests

--- src/models/IContract.js
+++ src/models/IContract.js
@@ -112,7 +112,7 @@ class IContract {
             // fetch should be non-blocking
           }
 
-          if (userOperationData?.data?.status === 'failed') {
+          if (userOperationData && userOperationData.data && userOperationData.data.status === 'failed') {
             clearInterval(interval);
             reject(new Error('User operation failed'));
           }
@@ -334,8 +334,8 @@ class IContract {
             }
           }
 
-          let userOp = feeQuotesResult.verifyingPaymasterGasless?.userOp;
-          let userOpHash = feeQuotesResult.verifyingPaymasterGasless?.userOpHash;
+          let userOp = feeQuotesResult.verifyingPaymasterGasless.userOp;
+          let userOpHash = feeQuotesResult.verifyingPaymasterGasless.userOpHash;
 
           // Get random key
           const key = BigInt(Math.floor(Math.random() * 6277101735386680763835789423207666416102355444464034512895));
@@ -371,7 +371,7 @@ class IContract {
           );
 
 
-          userOp.paymasterAndData = paymasterSponsorData?.data.result.paymasterAndData;
+          userOp.paymasterAndData = paymasterSponsorData.data.result.paymasterAndData;
 
           userOpHash = this.getUserOpHash(networkConfig.chainId, userOp, ENTRYPOINT_ADDRESS_V06);
 
@@ -421,7 +421,7 @@ class IContract {
 
           const transactionHash = await this.waitForTransactionHashToBeGenerated(userOpHash, networkConfig);
 
-          const web3Provider = new ethers.providers.Web3Provider(smartAccount?.provider)
+          const web3Provider = new ethers.providers.Web3Provider(smartAccount.provider)
 
           receipt = await web3Provider.waitForTransaction(transactionHash);
         }
